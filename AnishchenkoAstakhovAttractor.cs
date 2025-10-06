69ba00a1be39f0841abc2c4346715bdf55b9a191
Felixyang777 <1761610526@qq.com>
2025-10-06 13:32:22 +0800

    AnishchenkoAstakhovAttractor

---
+++ AnishchenkoAstakhovAttractor.cs
@@ -0,0 +1,118 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ChaosTheory
+{
+    public class AnishchenkoAstakhovAttractor : GH_Component
+    {
+        public AnishchenkoAstakhovAttractor()
+          : base("AnishchenkoAstakhovAttractor", "AA",
+            "Construct a AnishchenkoAstakhovAttractor.",
+            "ChaosTheory", "Attractor")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+
+            pManager.AddPointParameter("StartPoint", "P", "StartPoint", GH_ParamAccess.item, new Point3d(1, 0, 0));
+            pManager.AddNumberParameter("Miu", "μ", "Miu", GH_ParamAccess.item, 1.2);
+            pManager.AddNumberParameter("Eta", "η", "Eta", GH_ParamAccess.item, 0.5);
+            pManager.AddNumberParameter("DeltaT", "Δt", "DeltaT", GH_ParamAccess.item, 0.01);
+            pManager.AddIntegerParameter("Iterations", "I", "Number of  iterations", GH_ParamAccess.item, 1000);
+
+        }
+
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+
+            pManager.AddPointParameter("Points", "P", "LorenzOscillator", GH_ParamAccess.list);
+            pManager.AddCurveParameter("Curve", "C", "LorenzOscillator", GH_ParamAccess.item);
+
+            //pManager.HideParameter(0);
+        }
+
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+            Point3d StartPoint = Point3d.Origin;
+            double Miu = 0.0;
+            double Eta = 0.0;
+            double DeltaT = 0.0;
+            int Iterations = 100;
+
+
+            if (!DA.GetData(0, ref StartPoint)) return;
+            if (!DA.GetData(1, ref Miu)) return;
+            if (!DA.GetData(2, ref Eta)) return;
+            if (!DA.GetData(3, ref DeltaT)) return;
+            if (!DA.GetData(4, ref Iterations)) return;
+
+            if (DeltaT <= 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "DeltaT must be positive");
+                return;
+            }
+
+            if (Iterations <= 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Iterations must be positive");
+                return;
+            }
+            List<Point3d> AnishchenkoAstakhovAttractorPoints = GenerateAnishchenkoAstakhovAttractor(StartPoint, Miu, Eta, DeltaT, Iterations);
+            IEnumerable __enum_points = (IEnumerable)AnishchenkoAstakhovAttractorPoints;
+            DA.SetDataList(0, __enum_points);
+
+            var curve = Curve.CreateInterpolatedCurve(AnishchenkoAstakhovAttractorPoints, 3);
+            DA.SetData(1, curve);
+
+        }
+
+        List<Point3d> newpoints;
+        Point3d point;
+        List<Point3d> GenerateAnishchenkoAstakhovAttractor(Point3d StartPoint, double Miu, double Eta, double DeltaT, int Iterations)
+        {
+            point = StartPoint;
+            newpoints = new List<Point3d>();
+
+            double x = point.X;
+            double y = point.Y;
+            double z = point.Z;
+
+
+
+            for (int i = 0; i < Iterations; i++)
+            {
+
+                newpoints.Add(point);
+
+                int Ix = (x < 0) ? 1 : 0;
+                double dx = Miu+x+y-x*z;
+                double dy = -x;  
+                double dz = -Eta *z+Eta*Ix*Math.Pow(x,2);
+
+                x += dx * DeltaT;
+                y += dy * DeltaT;
+                z += dz * DeltaT;
+
+                point = new Point3d(x, y, z);
+            }
+
+
+            return newpoints;
+
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        protected override System.Drawing.Bitmap Icon => null;
+
+        public override Guid ComponentGuid => new Guid("F27A5E3C-9B84-4D12-A6C5-8E3F1B7D4A29");
+    }
+}
