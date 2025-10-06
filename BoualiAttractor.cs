d7ab9df9a4d04bfdc2e94171f02608517004c332
Felixyang777 <1761610526@qq.com>
2025-10-06 14:27:38 +0800

    BoualiAttractor

---
+++ BoualiAttractor.cs
@@ -0,0 +1,115 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ChaosTheory
+{
+    public class BoualiAttractor : GH_Component
+    {
+        public BoualiAttractor()
+          : base("BoualiAttractor", "AA",
+            "Construct a BoualiAttractor.",
+            "ChaosTheory", "Attractor")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+
+            pManager.AddPointParameter("StartPoint", "P", "StartPoint", GH_ParamAccess.item, new Point3d(1, 0, 0));
+            pManager.AddNumberParameter("Alpha", "α", "Alpha", GH_ParamAccess.item, 0.3);
+            pManager.AddNumberParameter("Delta", "δ", "Delta", GH_ParamAccess.item, 1);
+            pManager.AddNumberParameter("DeltaT", "Δt", "DeltaT", GH_ParamAccess.item, 0.01);
+            pManager.AddIntegerParameter("Iterations", "I", "Number of  iterations", GH_ParamAccess.item, 1000);
+
+        }
+
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+
+            pManager.AddPointParameter("Points", "P", "LorenzOscillator", GH_ParamAccess.list);
+            pManager.AddCurveParameter("Curve", "C", "LorenzOscillator", GH_ParamAccess.item);
+
+            //pManager.HideParameter(0);
+        }
+
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+            Point3d StartPoint = Point3d.Origin;
+            double Alpha = 0.0;
+            double Delta = 0.0;
+            double DeltaT = 0.0;
+            int Iterations = 100;
+
+            if (!DA.GetData(0, ref StartPoint)) return;
+            if (!DA.GetData(1, ref Alpha)) return;
+            if (!DA.GetData(2, ref Delta)) return;
+            if (!DA.GetData(3, ref DeltaT)) return;
+            if (!DA.GetData(4, ref Iterations)) return;
+
+            if (DeltaT <= 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "DeltaT must be positive");
+                return;
+            }
+
+            if (Iterations <= 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Iterations must be positive");
+                return;
+            }
+            List<Point3d> BoualiAttractorPoints = GenerateBoualiAttractor(StartPoint, Alpha, Delta, DeltaT, Iterations);
+            IEnumerable __enum_points = (IEnumerable)BoualiAttractorPoints;
+            DA.SetDataList(0, __enum_points);
+
+            var curve = Curve.CreateInterpolatedCurve(BoualiAttractorPoints, 3);
+            DA.SetData(1, curve);
+
+        }
+
+        List<Point3d> newpoints;
+        Point3d point;
+        List<Point3d> GenerateBoualiAttractor(Point3d StartPoint, double Alpha, double Delta, double DeltaT, int Iterations)
+        {
+            point = StartPoint;
+            newpoints = new List<Point3d>();
+
+            double x = point.X;
+            double y = point.Y;
+            double z = point.Z;
+
+
+
+            for (int i = 0; i < Iterations; i++)
+            {
+
+                newpoints.Add(point);
+                double dx = x * (4 - y) + Alpha * z;
+                double dy = -y * (1 - Math.Pow(x, 2));
+                double dz = -x * (1.5 - Delta * z) - 0.05 * z;
+
+                x += dx * DeltaT;
+                y += dy * DeltaT;
+                z += dz * DeltaT;
+
+                point = new Point3d(x, y, z);
+            }
+
+
+            return newpoints;
+
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        protected override System.Drawing.Bitmap Icon => null;
+
+        public override Guid ComponentGuid => new Guid("3B8A1C9E-7D4F-4A2B-9C6D-5E3F8A1B4C7D");
+    }
+}
