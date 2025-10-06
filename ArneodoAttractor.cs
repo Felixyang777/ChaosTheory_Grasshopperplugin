a6665aee16012fed73558f6a51e6128f5c520501
Felixyang777 <1761610526@qq.com>
2025-10-06 13:48:59 +0800

    ArneodoAttractor

---
+++ ArneodoAttractor.cs
@@ -0,0 +1,123 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ChaosTheory
+{
+    public class ArneodoAttractor : GH_Component
+    {
+        public ArneodoAttractor()
+          : base("ArneodoAttractor", "AA",
+            "Construct a ArneodoAttractor.",
+            "ChaosTheory", "Attractor")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+
+            pManager.AddPointParameter("StartPoint", "P", "StartPoint", GH_ParamAccess.item, new Point3d(1, 0, 0));
+            pManager.AddNumberParameter("Alpha", "α", "Alpha", GH_ParamAccess.item, -5.5);
+            pManager.AddNumberParameter("Beta", "β", "Beta", GH_ParamAccess.item, 3.5);
+            pManager.AddNumberParameter("Delta", "δ", "Delta", GH_ParamAccess.item, -1);
+            pManager.AddNumberParameter("DeltaT", "Δt", "DeltaT", GH_ParamAccess.item, 0.01);
+            pManager.AddIntegerParameter("Iterations", "I", "Number of  iterations", GH_ParamAccess.item, 1000);
+
+        }
+
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+
+            pManager.AddPointParameter("Points", "P", "LorenzOscillator", GH_ParamAccess.list);
+            pManager.AddCurveParameter("Curve", "C", "LorenzOscillator", GH_ParamAccess.item);
+
+            //pManager.HideParameter(0);
+        }
+
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+            Point3d StartPoint = Point3d.Origin;
+            double Alpha = 0.0;
+            double Beta = 0.0;
+            double Gama = 0.0;
+            double Delta = 0.0;
+            double Epsilon = 0.0;
+            double Zeta = 0.0;
+            double DeltaT = 0.0;
+            int Iterations = 100;
+
+
+            if (!DA.GetData(0, ref StartPoint)) return;
+            if (!DA.GetData(1, ref Alpha)) return;
+            if (!DA.GetData(2, ref Beta)) return;
+            if (!DA.GetData(3, ref Delta)) return;
+            if (!DA.GetData(4, ref DeltaT)) return;
+            if (!DA.GetData(5, ref Iterations)) return;
+
+            if (DeltaT <= 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "DeltaT must be positive");
+                return;
+            }
+
+            if (Iterations <= 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Iterations must be positive");
+                return;
+            }
+
+            List<Point3d> ArneodoAttractorPoints = GenerateArneodoAttractor(StartPoint, Alpha, Beta,  Delta,DeltaT, Iterations);
+            IEnumerable __enum_points = (IEnumerable)ArneodoAttractorPoints;
+            DA.SetDataList(0, __enum_points);
+
+            var curve = Curve.CreateInterpolatedCurve(ArneodoAttractorPoints, 3);
+            DA.SetData(1, curve);
+
+        }
+
+        List<Point3d> newpoints;
+        Point3d point;
+        List<Point3d> GenerateArneodoAttractor(Point3d StartPoint, double Alpha, double Beta,  double Delta, double DeltaT, int Iterations)
+        {
+            point = StartPoint;
+            newpoints = new List<Point3d>();
+
+            double x = point.X;
+            double y = point.Y;
+            double z = point.Z;
+
+
+
+            for (int i = 0; i < Iterations; i++)
+            {
+
+                newpoints.Add(point);
+                double dx =  y;
+                double dy = z;
+                double dz = -Alpha*x-Beta*y-z+Delta*Math.Pow(x,3);
+
+                x += dx * DeltaT;
+                y += dy * DeltaT;
+                z += dz * DeltaT;
+
+                point = new Point3d(x, y, z);
+            }
+
+
+            return newpoints;
+
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        protected override System.Drawing.Bitmap Icon => null;
+
+        public override Guid ComponentGuid => new Guid("6C9D2E8A-1F4B-4A3C-9D7E-5F3A8B1C4E6D");
+    }
+}
