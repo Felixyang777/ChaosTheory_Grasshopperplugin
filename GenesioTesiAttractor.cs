53efcc6f35f3cc8d565d378f028550880883dfb1
Felixyang777 <1761610526@qq.com>
2025-10-06 20:06:55 +0800

    GenesioTesiAttractor

---
+++ GenesioTesiAttractor.cs
@@ -0,0 +1,119 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ChaosTheory
+{
+    public class GenesioTesiAttractor : GH_Component
+    {
+        public GenesioTesiAttractor()
+          : base("GenesioTesiAttractor", "AA",
+            "Construct a GenesioTesiAttractor.",
+            "ChaosTheory", "Attractor")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+
+            pManager.AddPointParameter("StartPoint", "P", "StartPoint", GH_ParamAccess.item, new Point3d(0.1, 0.2, 0.6));
+            pManager.AddNumberParameter("Alpha", "α", "Alpha", GH_ParamAccess.item, 0.44);
+            pManager.AddNumberParameter("Beta", "β", "Beta", GH_ParamAccess.item, 1.1);
+            pManager.AddNumberParameter("Sigma", "ς", "Sigma", GH_ParamAccess.item, 1);
+            pManager.AddNumberParameter("DeltaT", "Δt", "DeltaT", GH_ParamAccess.item, 0.001);
+            pManager.AddIntegerParameter("Iterations", "I", "Number of  iterations", GH_ParamAccess.item, 100000);
+
+        }
+
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+
+            pManager.AddPointParameter("Points", "P", "LorenzOscillator", GH_ParamAccess.list);
+            pManager.AddCurveParameter("Curve", "C", "LorenzOscillator", GH_ParamAccess.item);
+
+            //pManager.HideParameter(0);
+        }
+
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+            Point3d StartPoint = Point3d.Origin;
+            double Alpha = 0.0;
+            double Beta = 0.0;
+            double Sigma = 0.0;
+            double DeltaT = 0.0;
+            int Iterations = 100;
+
+
+            if (!DA.GetData(0, ref StartPoint)) return;
+            if (!DA.GetData(1, ref Alpha)) return;
+            if (!DA.GetData(2, ref Beta)) return;
+            if (!DA.GetData(3, ref Sigma)) return;
+            if (!DA.GetData(4, ref DeltaT)) return;
+            if (!DA.GetData(5, ref Iterations)) return;
+
+            if (DeltaT <= 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "DeltaT must be positive");
+                return;
+            }
+
+            if (Iterations <= 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Iterations must be positive");
+                return;
+            }
+            List<Point3d> GenesioTesiAttractorPoints = GenerateGenesioTesiAttractor(StartPoint, Alpha, Beta, Sigma, DeltaT, Iterations);
+            IEnumerable __enum_points = (IEnumerable)GenesioTesiAttractorPoints;
+            DA.SetDataList(0, __enum_points);
+
+            var curve = Curve.CreateInterpolatedCurve(GenesioTesiAttractorPoints, 3);
+            DA.SetData(1, curve);
+
+        }
+
+        List<Point3d> newpoints;
+        Point3d point;
+        List<Point3d> GenerateGenesioTesiAttractor(Point3d StartPoint, double Alpha, double Beta, double Sigma, double DeltaT, int Iterations)
+        {
+            point = StartPoint;
+            newpoints = new List<Point3d>();
+
+            double x = point.X;
+            double y = point.Y;
+            double z = point.Z;
+
+
+
+            for (int i = 0; i < Iterations; i++)
+            {
+
+                newpoints.Add(point);
+                double dx = y;
+                double dy = z;
+                double dz =-Sigma*x-Beta*y-Alpha*z+Math.Pow(x,2);
+
+                x += dx * DeltaT;
+                y += dy * DeltaT;
+                z += dz * DeltaT;
+
+                point = new Point3d(x, y, z);
+            }
+
+
+            return newpoints;
+
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        protected override System.Drawing.Bitmap Icon => null;
+
+        public override Guid ComponentGuid => new Guid("9D4A7C1E-3F8B-4A5C-9D2E-6B1A8C4F7E3D");
+    }
+}
