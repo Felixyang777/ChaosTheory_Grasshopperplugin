09fe4f4231fa8852f110dfc66d42d48bbfd91d87
Felixyang777 <1761610526@qq.com>
2025-10-06 15:22:13 +0800

    BurkeShawAttractor

---
+++ BurkeShawAttractor.cs
@@ -0,0 +1,116 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ChaosTheory
+{
+    public class BurkeShawAttractor : GH_Component
+    {
+        public BurkeShawAttractor()
+          : base("BurkeShawAttractor", "AA",
+            "Construct a BurkeShawAttractor.",
+            "ChaosTheory", "Attractor")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+
+            pManager.AddPointParameter("StartPoint", "P", "StartPoint", GH_ParamAccess.item, new Point3d(1, 0, 0));
+            pManager.AddNumberParameter("Sigma", "ς", "Sigma", GH_ParamAccess.item, 10);
+            pManager.AddNumberParameter("Nu", "ν", "Nu", GH_ParamAccess.item,4.272);
+            pManager.AddNumberParameter("DeltaT", "Δt", "DeltaT", GH_ParamAccess.item, 0.01);
+            pManager.AddIntegerParameter("Iterations", "I", "Number of  iterations", GH_ParamAccess.item, 1000);
+
+        }
+
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+
+            pManager.AddPointParameter("Points", "P", "LorenzOscillator", GH_ParamAccess.list);
+            pManager.AddCurveParameter("Curve", "C", "LorenzOscillator", GH_ParamAccess.item);
+
+            //pManager.HideParameter(0);
+        }
+
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+            Point3d StartPoint = Point3d.Origin;
+            double Sigma = 0.0;
+            double Nu = 0.0;
+            double DeltaT = 0.0;
+            int Iterations = 100;
+
+
+            if (!DA.GetData(0, ref StartPoint)) return;
+            if (!DA.GetData(1, ref Sigma)) return;
+            if (!DA.GetData(2, ref Nu)) return;
+            if (!DA.GetData(3, ref DeltaT)) return;
+            if (!DA.GetData(4, ref Iterations)) return;
+
+            if (DeltaT <= 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "DeltaT must be positive");
+                return;
+            }
+
+            if (Iterations <= 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Iterations must be positive");
+                return;
+            }
+            List<Point3d> BurkeShawAttractorPoints = GenerateBurkeShawAttractor(StartPoint, Sigma, Nu, DeltaT, Iterations);
+            IEnumerable __enum_points = (IEnumerable)BurkeShawAttractorPoints;
+            DA.SetDataList(0, __enum_points);
+
+            var curve = Curve.CreateInterpolatedCurve(BurkeShawAttractorPoints, 3);
+            DA.SetData(1, curve);
+
+        }
+
+        List<Point3d> newpoints;
+        Point3d point;
+        List<Point3d> GenerateBurkeShawAttractor(Point3d StartPoint, double Sigma, double Nu, double DeltaT, int Iterations)
+        {
+            point = StartPoint;
+            newpoints = new List<Point3d>();
+
+            double x = point.X;
+            double y = point.Y;
+            double z = point.Z;
+
+
+
+            for (int i = 0; i < Iterations; i++)
+            {
+
+                newpoints.Add(point);
+                double dx = -Sigma*(x+y);
+                double dy = -y-Sigma*x*z;
+                double dz = Sigma*x*y+Nu;
+
+                x += dx * DeltaT;
+                y += dy * DeltaT;
+                z += dz * DeltaT;
+
+                point = new Point3d(x, y, z);
+            }
+
+
+            return newpoints;
+
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        protected override System.Drawing.Bitmap Icon => null;
+
+        public override Guid ComponentGuid => new Guid("9E4A7C1D-3F8B-4A5C-9D2E-6B1A8C4F7E3D");
+    }
+}
