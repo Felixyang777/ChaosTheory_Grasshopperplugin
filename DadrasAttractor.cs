26347cdd42fd1dac685d82416cf11377ed9fcbd8
Felixyang777 <1761610526@qq.com>
2025-10-07 09:50:58 +0800

    DadrasAttractor

--- DadrasAttractor.cs
+++ DadrasAttractor.cs
@@ -23,7 +23,7 @@ namespace ChaosTheory
             pManager.AddNumberParameter("Rou", "ρ", "Rou", GH_ParamAccess.item, 3);
             pManager.AddNumberParameter("Sigma", "σ", "Sigma", GH_ParamAccess.item, 2.7);
             pManager.AddNumberParameter("Gama", "γ", "Gama", GH_ParamAccess.item, 1.7);
-            pManager.AddNumberParameter("Sigma", "ς", "Sigma", GH_ParamAccess.item, 2);
+            pManager.AddNumberParameter("Delta", "δ", "Delta", GH_ParamAccess.item, 2);
             pManager.AddNumberParameter("Epsilon", "ε", "Epsilon", GH_ParamAccess.item, 9);
             pManager.AddNumberParameter("DeltaT", "Δt", "DeltaT", GH_ParamAccess.item, 0.01);
             pManager.AddIntegerParameter("Iterations", "I", "Number of  iterations", GH_ParamAccess.item, 1000);
@@ -46,9 +46,9 @@ namespace ChaosTheory
 
             Point3d StartPoint = Point3d.Origin;
             double Rou = 0.0;
-            double Sigma1 = 0.0;
+            double Sigma = 0.0;
             double Gama = 0.0;
-            double Sigma2 = 0.0;
+            double Delta = 0.0;
             double Epsilon = 0.0;
             double DeltaT = 0.0;
             int Iterations = 100;
@@ -56,9 +56,9 @@ namespace ChaosTheory
 
             if (!DA.GetData(0, ref StartPoint)) return;
             if (!DA.GetData(1, ref Rou)) return;
-            if (!DA.GetData(2, ref Sigma1)) return;
+            if (!DA.GetData(2, ref Sigma)) return;
             if (!DA.GetData(3, ref Gama)) return;
-            if (!DA.GetData(4, ref Sigma2)) return;
+            if (!DA.GetData(4, ref Delta)) return;
             if (!DA.GetData(5, ref Epsilon)) return;
             if (!DA.GetData(6, ref DeltaT)) return;
             if (!DA.GetData(7, ref Iterations)) return;
@@ -74,7 +74,7 @@ namespace ChaosTheory
                 AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Iterations must be positive");
                 return;
             }
-            List<Point3d> LorenzOscillatorPoints = GenerateDadrasAttractor(StartPoint, Rou, Sigma1, Gama, Sigma2, Epsilon, DeltaT, Iterations);
+            List<Point3d> LorenzOscillatorPoints = GenerateDadrasAttractor(StartPoint, Rou, Sigma, Gama, Delta, Epsilon, DeltaT, Iterations);
             IEnumerable __enum_points = (IEnumerable)LorenzOscillatorPoints;
             DA.SetDataList(0, __enum_points);
 
@@ -85,7 +85,7 @@ namespace ChaosTheory
 
         List<Point3d> newpoints;
         Point3d point;
-        List<Point3d> GenerateDadrasAttractor(Point3d StartPoint, double Rou, double Sigma1, double Gama, double Sigma2, double Epsilon, double DeltaT, int Iterations)
+        List<Point3d> GenerateDadrasAttractor(Point3d StartPoint, double Rou, double Sigma, double Gama, double Delta, double Epsilon, double DeltaT, int Iterations)
         {
             point = StartPoint;
             newpoints = new List<Point3d>();
@@ -100,9 +100,9 @@ namespace ChaosTheory
             {
 
                 newpoints.Add(point);
-                double dx = y-Rou*x+Sigma1*y*z;
+                double dx = y-Rou*x+Sigma*y*z;
                 double dy = Gama*y-x*z+z;
-                double dz = Sigma2*x*y-Epsilon*z;
+                double dz = Delta*x*y-Epsilon*z;
 
                 x += dx * DeltaT;
                 y += dy * DeltaT;
